002f7e109ca7f5919f46eaef9076d2c6fb51a873
alinetiuss <169068304+alinetiuss@users.noreply.github.com>
2024-05-13 14:37:35 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+ import static org.junit.jupiter.api.Assertions.assertEquals;
+
+ import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    system.out.println("#################");
+    System.out.println("# Billie Eilish #");
+    system.out.println("#################");
+    
+    system.out.println("");
+
+    system.out.println("Álbum: Happier Than Ever ");
+    system.out.println("lançamento 30 de julho de 2021");
+
+      system.out.println("");
+    
+    system.out.println(" Happier Than Ever é o segundo álbum de estúdio da cantora estadunidense Billie Eilish, lançado em 30 de julho de 2021 através da Darkroom e Interscope Records. Eilish escreveu o álbum com seu colaborador frequente, seu irmão Finneas O'Connell, que produziu o álbum sozinho. Ela citou a autorreflexão durante a pandemia de COVID-19 como a maior inspiração para o disco. ");
+
+    system.out.println("###########");
+    system.out.println("# Músicas #");
+    system.out.println("###########");
+
+    system.out.println("")
+      
+  }
+  @Test
+   void addition() {
+       assertEquals(2, 1 + 1);
+   }
+}
