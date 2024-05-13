4b23790b0b29172feb4dfb8e99a8c3719371ead4
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-05-13 21:26:59 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,34 @@
 public class Main {
   public static void main(String[] args) {
-    system.out.println("#################");
-    System.out.println("# Billie Eilish #");
-    system.out.println("#################");
-    
-    system.out.println("");
-
-    system.out.println(" Álbum: Happier Than Ever ");
-    system.out.println("Lançamento 30 de julho de 2021");
-
-      system.out.println("Billie Eilish Pirate Baird O'Connell nasceu em Los Angeles, Califórnia, em 18 de dezembro de 2001, filha de Maggie Baird e Patrick O'Connell, ambos conhecidos na indústria do entretenimento.Ela é descendente de irlandeses e escoceses e foi criada no Highland Park de Los Angeles. Eilish foi educada em casa e ingressou no Coro Infantil de Los Angeles aos oito anos. Ela começou a cantar em tenra idade e começou a escrever músicas aos 11 anos, ao lado de seu irmão mais velho, Finneas O'Connell, que já estava escrevendo, tocando e produzindo suas próprias canções com sua banda.");
-    
-    system.out.println("");
-
-    system.out.println("###########");
-    system.out.println("# Músicas #");
-    system.out.println("###########");
-
-    system.out.println(" Título                      Tempo ");
-    system.out.println(" Getting Older                4:00 ");
-    system.out.println(" I Didn't Change My Number    2:38 ");
-    system.out.println(" Billie Bossa Nova            3:16 ");
-    system.out.println("My future                     3:30 ");
-    
+    System.out.println("                        Billie Eilish!");
+    System.out.println("");
+    System.out.println("                   Álbum: Happier Than Ever ");
+    System.out.println("                   Lançamento 30 de julho de 2021");
+    System.out.println("");
+    System.out.println("   Billie Eilish Pirate Baird O'Connell nasceu em Los Angeles, Califórnia, em 18 de dezembro de 2001, filha de Maggie Baird e Patrick O'Connell, ambos conhecidos na indústria do entretenimento.Ela é descendente de irlandeses e escoceses e foi criada no Highland Park de Los Angeles. Eilish foi educada em casa e ingressou no Coro Infantil de Los Angeles aos oito anos. Ela começou a cantar em tenra idade e começou a escrever músicas aos 11 anos, ao lado de seu irmão mais velho, Finneas O'Connell, que já estava escrevendo, tocando e produzindo suas próprias canções com sua banda."); 
+    System.out.println("");
+    System.out.println("                           ----------- ");
+    System.out.println("                          |  Músicas  |");
+    System.out.println("                           ----------- ");
+    System.out.println("");
+    System.out.println("");
+    System.out.println(" |             Nome            | Tempo |");
+    System.out.println(" |-----------------------------|-------|");
+    System.out.println(" | Getting Older               | 4:00  |");
+    System.out.println(" | I Didn't Change My Number   | 2:38  |");
+    System.out.println(" | Billie Bossa Nova           | 3:16  |");
+    System.out.println(" | My future                   | 3:30  |");
+    System.out.println(" | Oxytocin                    | 3:30  |");
+    System.out.println(" | Goldwing                    | 2:31  |");
+    System.out.println(" | Lost Cause                  | 3:32  |");
+    System.out.println(" | Halley's Comet              | 3:54  |");
+    System.out.println(" | Not My Responsibility       | 3:47  |");
+    System.out.println(" | OverHeated                  | 3:34  |");
+    System.out.println(" | Everybody Dies              | 3:26  |");
+    System.out.println(" | Your Power                  | 4:05  |");
+    System.out.println(" | NDA                         | 3:15  |");
+    System.out.println(" | Therefore I Am              | 2:53  |");
+    System.out.println(" | Happier Than Ever           | 4:58  |");
+    System.out.println(" | Male Fantasy                | 3:14  |");
+  }
+}
