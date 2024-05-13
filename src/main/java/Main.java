662cd61c799f12f0b6210b146592cc3ab4ab304e
alinetiuss <169068304+alinetiuss@users.noreply.github.com>
2024-05-13 14:58:10 +0000

    13 de maio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,3 @@
- import static org.junit.jupiter.api.Assertions.assertEquals;
-
- import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
     system.out.println("#################");
@@ -10,22 +6,20 @@ public class Main {
     
     system.out.println("");
 
-    system.out.println("Álbum: Happier Than Ever ");
-    system.out.println("lançamento 30 de julho de 2021");
+    system.out.println(" Álbum: Happier Than Ever ");
+    system.out.println("Lançamento 30 de julho de 2021");
 
-      system.out.println("");
+      system.out.println("Billie Eilish Pirate Baird O'Connell nasceu em Los Angeles, Califórnia, em 18 de dezembro de 2001, filha de Maggie Baird e Patrick O'Connell, ambos conhecidos na indústria do entretenimento.Ela é descendente de irlandeses e escoceses e foi criada no Highland Park de Los Angeles. Eilish foi educada em casa e ingressou no Coro Infantil de Los Angeles aos oito anos. Ela começou a cantar em tenra idade e começou a escrever músicas aos 11 anos, ao lado de seu irmão mais velho, Finneas O'Connell, que já estava escrevendo, tocando e produzindo suas próprias canções com sua banda.");
     
-    system.out.println(" Happier Than Ever é o segundo álbum de estúdio da cantora estadunidense Billie Eilish, lançado em 30 de julho de 2021 através da Darkroom e Interscope Records. Eilish escreveu o álbum com seu colaborador frequente, seu irmão Finneas O'Connell, que produziu o álbum sozinho. Ela citou a autorreflexão durante a pandemia de COVID-19 como a maior inspiração para o disco. ");
+    system.out.println("");
 
     system.out.println("###########");
     system.out.println("# Músicas #");
     system.out.println("###########");
 
-    system.out.println("")
-      
-  }
-  @Test
-   void addition() {
-       assertEquals(2, 1 + 1);
-   }
-}
+    system.out.println(" Título                      Tempo ");
+    system.out.println(" Getting Older                4:00 ");
+    system.out.println(" I Didn't Change My Number    2:38 ");
+    system.out.println(" Billie Bossa Nova            3:16 ");
+    system.out.println("My future                     3:30 ");
+    
